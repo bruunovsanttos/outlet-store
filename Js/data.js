82ee4998101b8f9d0a6b8e3c41e3384ac20c88fc
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-17 17:28:56 -0300

    criação de arquivos js como também base de datos de produtos para teste

---
+++ Js/data.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id:1,
+        name: "Açai 300ml",
+        description: "Açai tradicional com até 2 acompanhamentos.",
+        price: 15.00,
+        category: "Açai"
+    },
+
+    {
+        id:2,
+        name: "Açai 500ml",
+        description: "Açai tradicional com até 3 acompanhamentos.",
+        price: 25.00,
+        category: "Açai"
+    },
+    
+    {
+        id:3,
+        name: "Combo Casal",
+        description: "2 açais de 500 ml com acompanhamentos.",
+        price: 40.00,
+        category: "Combo"
+    }
+];
