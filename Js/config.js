4fc33d44c26a92ec6998d62824d9a8e44f6d5dfc
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-25 22:10:00 -0300

    adicionando configuração para troca de loja mais rapido

---
+++ Js/config.js
@@ -0,0 +1,7 @@
+const company = {
+    name: "Açaí Top",
+    slogan: "Monte seu pedido e receba no conforto da sua casa",
+    whatsapp: "5511999999999",
+    primaryColor: "#4b006e",
+    secondaryColor: "#f3e5f5"
+};
