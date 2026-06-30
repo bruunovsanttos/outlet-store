9a2a837ec53bddcf3447f412a6d28d41e306de86
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-30 00:59:45 -0300

    Adicionando variaveis para adição de fotos nos produtos

--- Js/data.js
+++ Js/data.js
@@ -4,7 +4,10 @@ const products = [
         name: "Açai 300ml",
         description: "Açai tradicional com até 2 acompanhamentos.",
         price: 15.00,
-        category: "Açai"
+        category: "Açai",
+        image: ,
+        badge: "Mais vendido",
+        ratting: 4.9
     },
 
     {
@@ -12,7 +15,10 @@ const products = [
         name: "Açai 500ml",
         description: "Açai tradicional com até 3 acompanhamentos.",
         price: 25.00,
-        category: "Açai"
+        category: "Açai",
+        image: ,
+        badge: "Mais vendido",
+        ratting: 4.9
     },
     
     {
@@ -20,6 +26,19 @@ const products = [
         name: "Combo Casal",
         description: "2 açais de 500 ml com acompanhamentos.",
         price: 40.00,
-        category: "Combo"
+        category: "Combo",
+        image: ,
+        badge: "Mais vendido",
+        ratting: 4.9
+    },
+    {
+        id: 4,
+        name: "Coca-Cola 2L",
+        description: "Refrigerante gelado para acompanhar seu pedido.",
+        price: 14.90,
+        category: "Bebidas",
+        image: "assets/img/products/coca-cola.jpg",
+        badge: "Bebida",
+        rating: 4.9
     }
 ];
