bacfb88e5d1fa2d8e3ee5918337d4425fd673cc0
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-30 01:23:23 -0300

    mudando cart para que sejam exibidas as novas diretrizes dos produtos

--- Js/main.js
+++ Js/main.js
@@ -8,13 +8,27 @@ function renderProducts() {
         productCard.classList.add("product-card");
 
         productCard.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>${product.description}</p>
-            <p class="product-price">R$ ${formatPrice(product.price)}</p>
-            <button class="btn" onclick="addToCart(${product.id})">
-                Adicionar ao carrinho
-            </button>
-        `;
+            <div class="product-image">
+                <img src="${product.image}" alt="${product.name}">
+                <span class="product-badge">${product.badge}</span>
+            </div>
+
+            <div class="product-content">
+            <div class="product-rating">★★★★★ ${product.rating}</div>
+
+                <h3>${product.name}</h3>
+
+                <p>${product.description}</p>
+
+            <div class="product-footer">
+                <strong class="product-price">R$ ${formatPrice(product.price)}</strong>
+
+                <button class="btn product-btn" onclick="addToCart(${product.id})">
+                Adicionar
+                </button>
+            </div>
+            </div>
+`;
 
         productList.appendChild(productCard);
     });
