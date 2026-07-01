fb0cf0721ebd3da6d28e1ad22727dfdb763f4be7
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-07-01 03:52:49 -0300

    fiz: numero de whats app

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const company = {
     name: "Açaí Da Vila",
     slogan: "O melhor açai da cidade agora sempre disponível na palma da sua mão.",
-    whatsapp: "5511999999999",
+    whatsapp: "5511949651772",
     primaryColor: "#4b006e",
     secondaryColor: "#f3e5f5"
 };
